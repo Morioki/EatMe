6f9187b7adc3887333961aa1347c5522de344880
Garrett Braatz <morioki@protonmail.com>
2023-11-25 21:24:32 -0800

    Adjust Tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,18 @@
 module.exports = {
     content: ['./EatMe.Client/**/*.{razor,html}',
                 './EatMe.Shared/**/*.{razor,html}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
     plugins: [
         require('@tailwindcss/typography'),
-        require('@tailwindcss/forms')
+        require('@tailwindcss/forms'),
+        require('daisyui')
     ],
+    daisyui: {
+        themes: ["dracula,light,dark,synthwave,cyberpunk,nord"],
+        darkTheme: "dracula"
+    },
 }
 
